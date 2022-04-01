80f6119489eacb921c7b28c31b3952444d7e8a95
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-04-01 15:03:20 +0700

    Ganti Swiper

--- src/assets/Image/Index.js
+++ src/assets/Image/Index.js
@@ -1,8 +1,8 @@
 import Logo from './logoKollApp.png'
 import Splash from './splashKollApp.png'
 import LogoPutih from './logoKollPutih.png'
-import Header1 from './SwiperHome-16.png'
-import Header2 from './SwiperHome-17.png'
-import Header3 from './SwiperHome-18.png'
+import Header1 from './SwiperKoll-19.png'
+import Header2 from './SwiperKoll-20.png'
+import Header3 from './SwiperKoll-21.png'
 
 export { Logo, Splash, LogoPutih, Header1, Header2, Header3 }
