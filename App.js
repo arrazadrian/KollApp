5a2a37d97e0ea819b5d59c78bf41e598e1713059
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-04-20 07:54:26 +0700

    20/04/2022 07.54 Auth Realm mulai

--- App.js
+++ App.js
@@ -3,9 +3,6 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import Routernih from './src/Router';
 
-//const express = require('express');
-const mongoose = require('mongoose');
-
 const App = () => {
   return (
     <NavigationContainer>
