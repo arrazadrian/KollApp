67eb859b1fb2ad923f08d73b3eae3aaff238a484
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-05-08 08:50:31 +0700

    UI HeaderTabs aman

--- src/Components/HeaderTabs.js
+++ src/Components/HeaderTabs.js
@@ -1,11 +1,25 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { Abu, Ijo, Putih } from '../Utils/Warna'
 
 const HeaderTabs = () => {
+  const [activeTab, setActiveTab] = useState("Dalam Proses");
   return (
-    <View>
-      <HeaderButton text="Dalam Proses"/>
-      <HeaderButton text="Sudah Selesai"/>
+    <View  style={{flexDirection:'row', alignSelf:'center'}}>
+      <HeaderButton 
+      text="Dalam Proses" 
+      btnColor= "Ijo" 
+      textColor="Putih"
+      activeTab={activeTab}
+      setActiveTab={setActiveTab}
+      />
+       <HeaderButton 
+      text="Sudah Selesai" 
+      btnColor= "Putih" 
+      textColor="Ijo"
+      activeTab={activeTab}
+      setActiveTab={setActiveTab}
+      />
     </View>
   )
 } 
@@ -14,18 +28,22 @@ export default HeaderTabs
 
 const HeaderButton = (props) => {
     return (
-        <View style={{flexDirection:'row', alignSelf:'center'}}>
-            <TouchableOpacity>
-                <Text>{props.text}</Text>
-            </TouchableOpacity>
-        </View>
+        <TouchableOpacity style={{
+          backgroundColor: props.activeTab === props.text ? Ijo : Putih, 
+          paddingVertical: 5,
+          paddingHorizontal: 10,
+          borderRadius: 10,
+          marginHorizontal: 3,
+          width: 155,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        onPress={() => props.setActiveTab(props.text)}
+        >
+            <Text style={{fontSize: 16, color: props.activeTab === props.text ? Putih : Ijo}}>{props.text}</Text>
+        </TouchableOpacity>
     )
 };
 
 
-const styles = StyleSheet.create({
-    tulisan:{
-        fontSize: 20,
-        
-    },
-})
+const styles = StyleSheet.create({})
--- src/Screens/RiwayatScreen.js
+++ src/Screens/RiwayatScreen.js
@@ -1,12 +1,15 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react'
-import { Ijo } from '../Utils/Warna'
+import { Ijo, Putih } from '../Utils/Warna'
+import HeaderTabs from '../Components/HeaderTabs'
 
 const RiwayatScreen = () => {
   return (
     <SafeAreaView>
       <View style={styles.container}>
-        <Text>RiwayatScreen</Text>
+        <View style={styles.bungkus}>
+          <HeaderTabs/>
+        </View>
       </View>
     </SafeAreaView>
   )
@@ -17,5 +20,16 @@ export default RiwayatScreen
 const styles = StyleSheet.create({
   container: {
     backgroundColor: Ijo,
+    paddingTop: 30,
+    paddingBottom: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bungkus: {
+    backgroundColor: Putih,
+    borderRadius: 10,
+    width: '80%',
+    height: 40,
+    paddingVertical: 5, 
   }
 })
