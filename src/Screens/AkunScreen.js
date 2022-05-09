625784ee998464ecd105ddaaf4e2536199605d28
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-05-09 14:49:42 +0700

    Logout tombol

--- src/Screens/AkunScreen.js
+++ src/Screens/AkunScreen.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react'
-import { Ijo, IjoTua, Kuning, Putih} from '../Utils/Warna';
+import { Ijo, IjoTua, Kuning, Merah, Putih} from '../Utils/Warna';
 
 const AkunScreen = () => {
   return (
@@ -18,19 +18,22 @@ const AkunScreen = () => {
             <View style={{borderBottomColor: Ijo, borderBottomWidth: 1}}>
               <Text style={{color: Putih, fontSize: 30, fontWeight: 'bold'}}>Info</Text>
             </View>
-            <View style={{padding: 10}}>
-                <View style={{justifyContent:"space-between", flexDirection:'row', marginBottom: 10}}>     
-                      <Text style={{color: Putih, fontSize: 20, fontWeight:'bold'}}>No.Handphone</Text> 
+            <View style={{padding: 15}}>
+                <View style={{justifyContent:"space-between", marginBottom: 10}}>     
+                      <Text style={{color: Putih, fontSize: 15, fontWeight:'bold'}}>No.Handphone</Text> 
                       <Text style={{color: Putih, fontSize: 20}}>0909090909090</Text>   
                 </View>
-                <View style={{justifyContent:"space-between", flexDirection:'row', marginBottom: 10}}>     
-                      <Text style={{color: Putih, fontSize: 20, fontWeight:'bold'}}>Email</Text> 
+                <View style={{justifyContent:"space-between", marginBottom: 10}}>     
+                      <Text style={{color: Putih, fontSize: 15, fontWeight:'bold'}}>Email</Text> 
                       <Text style={{color: Putih, fontSize: 20}}>contoh@gmail.com</Text>   
                 </View>
-                <View style={{justifyContent:"space-between", flexDirection:'row', marginBottom: 10}}>     
-                      <Text style={{color: Putih, fontSize: 20, fontWeight:'bold'}}>Alamat</Text> 
+                <View style={{justifyContent:"space-between", marginBottom: 10}}>     
+                      <Text style={{color: Putih, fontSize: 15, fontWeight:'bold'}}>Alamat</Text> 
                       <Text style={{color: Putih, fontSize: 20}}>Jl.Contoh aja</Text>   
                 </View>
+            </View>
+            <View style={styles.logout}>
+              <Text style={{fontSize: 20, color: Putih, fontWeight: 'bold'}}>Log Out</Text>
             </View>
       </View>
     </SafeAreaView>
@@ -64,5 +67,14 @@ const styles = StyleSheet.create({
   tulisan:{
     fontSize: 18,
     color: Putih,
+  },
+  logout:{
+    backgroundColor: Merah,
+    borderRadius: 10,
+    height: 40,
+    width: 300,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
   }
 })
--- src/Utils/Warna.js
+++ src/Utils/Warna.js
@@ -5,3 +5,4 @@ export const Putih = '#FFFFFF';
 export const Kuning = '#FFF3C8';
 export const Hitam = '#000000';
 export const Abu = '#DDDDDD';
+export const Merah = '#f06060';
