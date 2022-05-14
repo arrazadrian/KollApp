daffb98ea8ee90438843302ae17e5a5946750f2a
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-05-14 09:44:16 +0700

    Masuk Kategori Scree

--- src/Screens/PosisiScreen.js
+++ src/Screens/PosisiScreen.js
@@ -1,27 +1,32 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native'
 import React from 'react'
 import { Ijo, IjoTua, Kuning, Putih} from '../Utils/Warna';
 import { DPkartu } from '../assets/Image/Index.js'
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-const PosisiScreen = () => {
+const PosisiScreen = ({ navigation }) => {
   return (
     <View style={styles.latar}>
-      <View>
-          <Text>Peta</Text>
+      <View style={{height:'70%'}}>
+            <Text>Peta</Text>
       </View>
       <View style={styles.kotak}>
-          <View>
-            <Image source={DPkartu} style={styles.gambar}/>
-          </View>
-          <View style={{paddingHorizontal:20}}>
-            <Text style={{color: Putih, fontSize:20, fontWeight: 'bold'}}>Sayur Aa Anri</Text>
-            <Text style={{color: Putih, fontSize:16}}>
-                <Text>200m</Text>
-                <Text> | </Text>
-                <Text>20 min</Text>
-            </Text>
-          </View>
+            <View style={{flexDirection:'row'}}>
+                <View>
+                  <Image source={DPkartu} style={styles.gambar}/>
+                </View>
+                <View style={{paddingHorizontal:20}}>
+                  <Text style={{color: Putih, fontSize:20, fontWeight: 'bold'}}>Sayur Aa Anri</Text>
+                  <Text style={{color: Putih, fontSize:16}}>
+                      <Text>200m</Text>
+                      <Text> | </Text>
+                      <Text>20 min</Text>
+                  </Text>
+                  <Pressable style={styles.tombol} onPress={() => navigation.push('KategoriScreen')}>
+                    <Text style={{color: Putih, fontWeight: 'bold'}}>Lihat Produk</Text>
+                  </Pressable>
+                </View>
+            </View>
       </View>
     </View>
   )
@@ -37,12 +42,10 @@ const styles = StyleSheet.create({
     backgroundColor: Kuning,
   },
   kotak:{
-    flexDirection: 'row',
     backgroundColor: IjoTua,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    height:'50%',
-    top:250,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    height:'30%',
     padding: 20,
   },
   gambar:{
@@ -51,4 +54,11 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     padding: 20,
   },
+  tombol:{
+    backgroundColor: Ijo,
+    borderRadius: 10,
+    alignItems: 'center',
+    padding: 10,
+    marginTop: 10,
+  }
 })
