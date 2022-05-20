975d02809cca817e746e399c4d364a3f087d1e7a
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-05-20 15:08:50 +0700

    Modal melayang

--- src/Screens/KategoriScreen.js
+++ src/Screens/KategoriScreen.js
@@ -1,105 +1,115 @@
-import { Pressable, StyleSheet, Text, View, Image, ScrollView } from 'react-native'
+import { Pressable, StyleSheet, Text, View, Image, ScrollView, Modal } from 'react-native'
 import React from 'react'
-import { Ijo, IjoTua, Kuning, Putih } from '../Utils/Warna'
+import { Ijo, IjoMint, IjoTua, Kuning, Putih } from '../Utils/Warna'
 import { Kategori01, Kategori02, Kategori03, Kategori04,
         Kategori05, Kategori06, Kategori07, Kategori08,
         Kategori09, 
         KategoriPre, KollLong} from '../assets/Image/Index'
 import PencarianBar from '../Components/PencarianBar'
 import ListProduk from '../Components/ListProduk'
-import ListPreOrder from '../Components/ListPreOrder'
 
 const KategoriScreen = ({ navigation }) => {
   return (
-    <ScrollView style={styles.latar}>
-          <View style={{flexDirection: 'row', justifyContent:'space-between', alignItems:'center'}}>
-           <Image source={KollLong} style={{width: 80, height:50}} /> 
-          <PencarianBar/>
-          </View>
-          <View style={{marginBottom:10, marginLeft: 10}}>
-              <Text style={{fontSize: 20, fontWeight: 'bold', color: Ijo}}>Kategori</Text>
-          </View>
-          <View style={{flexDirection: 'row', alignItems:'center',justifyContent:'space-between'}}>
-          <Pressable style={styles.kartu}>
-            <View style={styles.belakang}>
-              <Image source={Kategori01} style={styles.gambar} />
-            </View>
-            <Text style={styles.nama}>Produk Laut</Text>
-          </Pressable>
-          <Pressable style={styles.kartu}>
-            <View style={styles.belakang}>
-              <Image source={Kategori02} style={styles.gambar} />
-            </View>
-            <Text style={styles.nama}>Daging</Text>
-          </Pressable>
-          <Pressable style={styles.kartu}>
-            <View style={styles.belakang}>
-              <Image source={Kategori03} style={styles.gambar} />
-            </View>
-            <Text style={styles.nama}>Buah</Text>
-          </Pressable>
-          </View>
-          <View style={{flexDirection: 'row', alignItems:'center',}}>
-          <Pressable style={styles.kartu}>
-            <View style={styles.belakang}>
-              <Image source={Kategori04} style={styles.gambar} />
-            </View>
-            <Text style={styles.nama}>Sayuran</Text>
-          </Pressable>
-          <Pressable style={styles.kartu}>
-            <View style={styles.belakang}>
-              <Image source={Kategori06} style={styles.gambar} />
-            </View>
-            <Text style={styles.nama}>Bahan Pokok</Text>
-          </Pressable>
-          <Pressable style={styles.kartu}>
-            <View style={styles.belakang}>
-              <Image source={Kategori05} style={styles.gambar} />
-            </View>
-            <Text style={styles.nama}>Cemilan</Text>
-          </Pressable>
-          </View>
-          <View style={{flexDirection: 'row', alignItems:'center',justifyContent:'space-between'}}>
-          <Pressable style={styles.kartu}>
-            <View style={styles.belakang}>
-              <Image source={Kategori07} style={styles.gambar} />
-            </View>
-            <Text style={styles.nama}>Lauk</Text>
-          </Pressable>
-          <Pressable style={styles.kartu}>
-            <View style={styles.belakang}>
-              <Image source={Kategori08} style={styles.gambar} />
-            </View>
-            <Text style={styles.nama}>Bumbu</Text>
-          </Pressable>
-          <Pressable style={styles.kartu}>
-            <View style={styles.belakang}>
-              <Image source={Kategori09} style={styles.gambar} />
-            </View>
-            <Text style={styles.nama}>Frozen Food</Text>
-          </Pressable>
-          </View>
-          <View style={{marginBottom:10, marginLeft: 10}}>
-              <Text style={{fontSize: 20, fontWeight: 'bold', color: Ijo}}>Tidak menemukan produk?</Text>
-          </View>
-          <Pressable style={styles.preorder} onPress={() => navigation.push('PreorderScreen')}>
+    <View style={styles.latar}>
+          <ScrollView>
+                <View style={{flexDirection: 'row', justifyContent:'space-between', alignItems:'center'}}>
+                <Image source={KollLong} style={{width: 80, height:50}} /> 
+                <PencarianBar/>
+                </View>
+                <View style={{marginBottom:10, marginLeft: 10}}>
+                    <Text style={{fontSize: 20, fontWeight: 'bold', color: Ijo}}>Kategori</Text>
+                </View>
+                <View style={{flexDirection: 'row', alignItems:'center',justifyContent:'space-between'}}>
+                <Pressable style={styles.kartu}>
+                  <View style={styles.belakang}>
+                    <Image source={Kategori01} style={styles.gambar} />
+                  </View>
+                  <Text style={styles.nama}>Produk Laut</Text>
+                </Pressable>
+                <Pressable style={styles.kartu}>
+                  <View style={styles.belakang}>
+                    <Image source={Kategori02} style={styles.gambar} />
+                  </View>
+                  <Text style={styles.nama}>Daging</Text>
+                </Pressable>
+                <Pressable style={styles.kartu}>
+                  <View style={styles.belakang}>
+                    <Image source={Kategori03} style={styles.gambar} />
+                  </View>
+                  <Text style={styles.nama}>Buah</Text>
+                </Pressable>
+                </View>
+                <View style={{flexDirection: 'row', alignItems:'center',}}>
+                <Pressable style={styles.kartu}>
+                  <View style={styles.belakang}>
+                    <Image source={Kategori04} style={styles.gambar} />
+                  </View>
+                  <Text style={styles.nama}>Sayuran</Text>
+                </Pressable>
+                <Pressable style={styles.kartu}>
+                  <View style={styles.belakang}>
+                    <Image source={Kategori06} style={styles.gambar} />
+                  </View>
+                  <Text style={styles.nama}>Bahan Pokok</Text>
+                </Pressable>
+                <Pressable style={styles.kartu}>
+                  <View style={styles.belakang}>
+                    <Image source={Kategori05} style={styles.gambar} />
+                  </View>
+                  <Text style={styles.nama}>Cemilan</Text>
+                </Pressable>
+                </View>
+                <View style={{flexDirection: 'row', alignItems:'center',justifyContent:'space-between'}}>
+                <Pressable style={styles.kartu}>
+                  <View style={styles.belakang}>
+                    <Image source={Kategori07} style={styles.gambar} />
+                  </View>
+                  <Text style={styles.nama}>Lauk</Text>
+                </Pressable>
+                <Pressable style={styles.kartu}>
+                  <View style={styles.belakang}>
+                    <Image source={Kategori08} style={styles.gambar} />
+                  </View>
+                  <Text style={styles.nama}>Bumbu</Text>
+                </Pressable>
+                <Pressable style={styles.kartu}>
+                  <View style={styles.belakang}>
+                    <Image source={Kategori09} style={styles.gambar} />
+                  </View>
+                  <Text style={styles.nama}>Frozen Food</Text>
+                </Pressable>
+                </View>
+                <View style={{marginBottom:10, marginLeft: 10}}>
+                    <Text style={{fontSize: 20, fontWeight: 'bold', color: Ijo}}>Tidak menemukan produk?</Text>
+                </View>
+                <Pressable style={styles.preorder} onPress={() => navigation.push('PreorderScreen')}>
+                  <View>
+                      <Text style={{fontSize: 20, fontWeight: 'bold', color: Ijo}}>Pre-Order</Text>
+                      <Text>Pesan produk yang belum dibawa mitra</Text>
+                  </View>
+                  <Image source={KategoriPre} style={styles.gambar} />
+                </Pressable>
+                <View style={{marginBottom:10, marginLeft: 10}}>
+                    <Text style={{fontSize: 20, fontWeight: 'bold', color: Ijo}}>Daftar Produk</Text>
+                </View>
+                <View style={{flexDirection:'row', flexWrap:'wrap'}}>
+                  <ListProduk/>
+                  <ListProduk/>
+                  <ListProduk/>
+                  <ListProduk/>
+                  <ListProduk/>
+                </View>
+          </ScrollView>
+          <View style={styles.panggil}>
             <View>
-                <Text style={{fontSize: 20, fontWeight: 'bold', color: Ijo}}>Pre-Order</Text>
-                <Text>Pesan produk yang belum dibawa mitra</Text>
+                <Text>Menemukan produk yang kamu mau?</Text>
+                <Text>Yuk panggil!</Text>
             </View>
-            <Image source={KategoriPre} style={styles.gambar} />
-          </Pressable>
-          <View style={{marginBottom:10, marginLeft: 10}}>
-              <Text style={{fontSize: 20, fontWeight: 'bold', color: Ijo}}>Daftar Produk</Text>
+            <View style={{padding: 10, backgroundColor: Ijo, borderRadius: 10}}>
+                <Text>Panggil Mitra</Text>
+            </View>
           </View>
-          <View style={{flexDirection:'row', flexWrap:'wrap'}}>
-             <ListProduk/>
-             <ListProduk/>
-             <ListProduk/>
-             <ListProduk/>
-             <ListProduk/>
-          </View>
-    </ScrollView>
+    </View>
   )
 }
 
@@ -109,7 +119,19 @@ const styles = StyleSheet.create({
   latar:{
     flex: 1,
     backgroundColor: Kuning,
-    padding: 10
+    paddingHorizontal: 10
+  },
+  panggil:{
+    flexDirection: 'row',
+    backgroundColor: IjoMint,
+    alignItems:'center',
+    justifyContent:'space-between',
+    padding: 10,
+    borderRadius: 10,
+    position: 'absolute',
+    width: '95%',
+    borderColor: IjoTua,
+    borderWidth: 1,
   },
   gambar:{
     width: 90,
@@ -121,7 +143,7 @@ const styles = StyleSheet.create({
     borderRadius: 10, 
     marginBottom: 5,
     borderWidth: 1,
-    borderColor: Ijo
+    borderColor: Ijo,
   },
   kartu:{
     width: '33%',
