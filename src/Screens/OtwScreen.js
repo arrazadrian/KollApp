e0c71b72e7358f121f6f703dc0fc412d679d7f90
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-05-31 17:34:48 +0700

    Gass

--- src/Screens/OtwScreen.js
+++ src/Screens/OtwScreen.js
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import MapView from 'react-native-maps'
+import { Kuning } from '../Utils/Warna'
+import { DPkartu } from '../assets/Image/Index'
 
 const OtwScreen = () => {
   return (
-    <View>
-      <Text>OtwScreen</Text>
+    <View style={styles.latar}>
+      <MapView style={styles.peta}/>
+      <View style={styles.bungkus}>
+          <Image source={DPkartu} style={styles.foto} />
+      </View>
     </View>
   )
 }
 
 export default OtwScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    latar:{
+        flex: 1,
+        backgroundColor: Kuning,
+    },
+    peta:{
+        width: '100%',
+        height: '75%',
+    },
+    bungkus:{
+        width: '100%',
+        height: '25%',
+        padding: 10,
+    },
+    foto:{
+        width: 100,
+        height: 100,
+        borderRadius: 20,
+    },
+})
