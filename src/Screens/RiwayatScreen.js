6dc7edb733ba6adf8ad8c44b0f494a93050761d5
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-06-01 17:47:40 +0700

    Gass component riwayat

--- src/Screens/RiwayatScreen.js
+++ src/Screens/RiwayatScreen.js
@@ -1,17 +1,21 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
-import { Ijo, Putih } from '../Utils/Warna'
+import { Ijo, Kuning, Putih } from '../Utils/Warna'
 import HeaderTabs from '../Components/HeaderTabs'
+import ProsesCard from '../Components/ProsesCard'
+import RiwayatCard from '../Components/RiwayatCard'
 
-const RiwayatScreen = () => {
+const RiwayatScreen = ({ navigation }) => {
   return (
-   
+    <ScrollView style={styles.latar}>
       <View style={styles.container}>
         <View style={styles.bungkus}>
           <HeaderTabs/>
         </View>
       </View>
-    
+      <ProsesCard/>
+      <RiwayatCard/>
+    </ScrollView>
   )
 }
 
@@ -30,5 +34,9 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     height: 40,
     paddingVertical: 5, 
-  }
+  },
+  latar:{
+    backgroundColor: Kuning,
+    flex: 1,
+  },
 })
