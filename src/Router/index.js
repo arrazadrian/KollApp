8048fa36a30de618ac8c4d751a5c1913cda226d5
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-06-03 14:55:17 +0700

    sikit

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -25,7 +25,6 @@ import {
   SplashScreen, 
 } from '../Screens/Index.js'
 import TabNavigasi from '../Components/TabNavigasi.js';
-import Kategori from '../Screens/KategoriScreen.js';
 import { Ijo, Putih } from '../Utils/Warna.js';
 
 
