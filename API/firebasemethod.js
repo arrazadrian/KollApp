c8acaa0d8821921638a5974af7d4dd263b5c09b4
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-08-08 14:42:37 +0700

    firebase masuk

---
+++ API/firebasemethod.js
@@ -0,0 +1,202 @@
+import { 
+    getAuth, 
+    onAuthStateChanged, 
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+   } from "firebase/auth";
+import { 
+  getFirestore, collection, 
+  addDoc, setDoc, doc, 
+  serverTimestamp, onSnapshot,
+  deleteDoc, getDoc, updateDoc,
+  } from 'firebase/firestore/lite';
+import { 
+  getStorage, ref, 
+  getDownloadURL, uploadBytes, 
+  deleteObject,
+  } from "firebase/storage";
+import { app } from "../Firebase/config";
+import {Alert} from "react-native";
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
+
+
+// API 1: registration
+// MEMBUAT AKUN BARU DENGAN EMAIL DAN PASSWORD, 
+// LALU MEMBUAT DOKUMEN BARU PADA COLLECTION MITRA
+
+export async function registration(email, password, namalengkap, namatoko, phone) {
+    const auth = getAuth();
+    const db = getFirestore(app);
+  try {
+    await createUserWithEmailAndPassword(auth, email, password)
+        .then(() => {
+            setDoc(doc(db, "mitra", auth.currentUser.uid),{
+                id_mitra: auth.currentUser.uid,
+                email: email,
+                namalengkap: namalengkap,
+                namatoko: namatoko,
+                phone: phone,
+                status_sekarang: "Tidak Aktif",
+            })
+        })
+  } catch (err) {
+    Alert.alert("Ada error membuat akun mitra!", err.message);
+  }
+};
+
+// API 2: signIn
+// MELAKUKAN LOGIN DENGAN EMAIL DAN PASSWORD,
+// AKAN ERROR KALO SALAH TULIS/AKUN TIDAK ADA
+
+export async function signIn(email, password) {
+  const auth = getAuth();
+  try { 
+    await signInWithEmailAndPassword(auth, email, password)
+  } catch (err) {
+    Alert.alert("User tidak ditemukan!", "Salah menulis email/kata sandi.");
+    }
+};
+
+// API 3: handleSignOut
+// KELUAR DARI DALAM AKUN YG SEDANG LOGIN,
+// MENGUBAH AUTHSTATECHANGE DAN KELUAR
+
+export async function handleSignOut() {
+  const auth = getAuth();
+  const db = getFirestore(app);
+  try {
+      signOut(auth);
+  } catch (err) {
+    Alert.alert('Ada error untuk keluar!', 'Tidak bisa keluar.');
+  }
+};
+
+// API 4: updateakunTanpafoto
+// PERBARUI DATA AKUN
+// DI FIRESTORE TANPA FOTO DI STORAGE
+
+export async function updateakunTanpafoto(namaakun, tokoakun, phoneakun){
+
+  const auth = getAuth();
+  const db = getFirestore(app);
+  // const docRef = doc(db, "mitra", auth.currentUser.uid);
+  // const colRef = collection(docRef, "produk")
+  // const storage = getStorage();
+  
+  const docrefproduk = doc(db, "mitra", auth.currentUser.uid);
+  getDoc(docrefproduk).then(docSnap => {
+    if (docSnap.exists()) {
+      try{
+        updateDoc(docrefproduk, {
+          namalengkap: namaakun,
+          namatoko: tokoakun,
+          phone: phoneakun,
+        });
+        Alert.alert(
+          'Data akun berhasil diperbarui','Data akunmu sudah terbarui.'
+        );
+      } catch (err) {
+        Alert.alert('Ada error untuk memperbarui data akunmu!', err.message);
+      }
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("Tidak ada dokumen tersebut!");
+    }
+  })
+}; 
+
+// API 5: updateakunDenganfoto
+// PERBARUI DATA AKUN
+// DI FIRESTORE DENGAN FOTO DI STORAGE
+
+export async function updateakunDenganfoto(fotoakun, namaakun, tokoakun, phoneakun){
+  const urlgambarbaru = await uploadgambarakun(fotoakun);
+
+  const auth = getAuth();
+  const db = getFirestore(app);
+
+  const storage = getStorage();
+  
+  const docrefproduk = doc(db, "mitra", auth.currentUser.uid);
+  getDoc(docrefproduk).then(docSnap => {
+    if (docSnap.exists()) {
+      if(docSnap.data().foto_akun) {
+        const imgURL =  docSnap.data().foto_akun;
+        const storageRef = ref(storage, imgURL);
+        try{
+          deleteObject(storageRef);
+          updateDoc(docrefproduk, {
+            foto_akun: urlgambarbaru,
+            namalengkap: namaakun,
+            namatoko: tokoakun,
+            phone: phoneakun,
+          });
+          Alert.alert(
+            'Data Akun Berhasil Diperbarui','Foto lama kamu juga sudah yang terbaru.'
+          );
+        } catch (err) {
+          Alert.alert('Ada error untuk memperbarui data akun!', err.message);
+        };
+      } else{
+        try{
+          updateDoc(docrefproduk, {
+            foto_akun: urlgambarbaru,
+            namalengkap: namaakun,
+            namatoko: tokoakun,
+            phone: phoneakun,
+          });
+          Alert.alert(
+            'Data Akun Berhasil Diperbarui','Data akunmu sudah terbarui.'
+          );
+        } catch (err) {
+          Alert.alert('Ada error untuk memperbarui data akun!', err.message);
+        };
+      }
+      
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("Tidak ada dokumen tersebut!");
+    }
+  })
+};
+
+// API 6: uploadgambarakun
+// UPLOAD FOTO AKUN YANG DIOPER KE
+// FUNGSI FIRESTORE SELANJUTNYA
+
+async function uploadgambarakun(uri) {
+  try{
+    const uploadUri = uri;
+    let extension = uploadUri.substring(uploadUri.lastIndexOf('.') + 1);
+
+    // Add uuid to File Name
+    filename = uuidv4() + '.' + extension;
+
+    // Why are we using XMLHttpRequest? See:
+    // https://github.com/expo/expo/issues/2402#issuecomment-443726662
+    const blob = await new Promise((resolve, reject) => {
+          const xhr = new XMLHttpRequest();
+          xhr.onload = function () {
+            resolve(xhr.response);
+          };
+          xhr.onerror = function (e) {
+            console.log(e);
+            reject(new TypeError("Network request failed"));
+          };
+          xhr.responseType = "blob";
+          xhr.open("GET", uri, true);
+          xhr.send(null);
+    });
+    const fileRef = ref(getStorage(app), `mitra/${filename}`);
+    const result = await uploadBytes(fileRef, blob);
+    
+    // We're done with the blob, close and release it
+    blob.close();
+    
+    return await getDownloadURL(fileRef);
+  } catch (err) {
+    Alert.alert('Ada error pada foto akun!', err.message);
+  }
+};
---
+++ Firebase/config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBnMmAnAw0o2SZ9AyakgE450XblGfadi0U",
+  authDomain: "koll-app.firebaseapp.com",
+  projectId: "koll-app",
+  storageBucket: "koll-app.appspot.com",
+  messagingSenderId: "485979616959",
+  appId: "1:485979616959:web:b2e01a3949a6a5ed96780a",
+  measurementId: "G-EVDPW5KD8M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
