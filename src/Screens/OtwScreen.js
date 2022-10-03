73fe715d0de1cdee879bf923866f440bc4b97232
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-10-03 13:35:05 +0700

    Gas tipis dulu

--- src/Screens/OtwScreen.js
+++ src/Screens/OtwScreen.js
@@ -4,6 +4,8 @@ import MapView, { Marker } from 'react-native-maps'
 import { Ijo, IjoMint, IjoTua, Kuning } from '../Utils/Warna'
 import { DPkartu } from '../assets/Images/Index'
 import { Call, Chat } from '../assets/Icons/Index'
+import GarisBatas from '../Components/GarisBatas';
+
 
 const { height, width } = Dimensions.get('window')
 
@@ -17,24 +19,24 @@ const OtwScreen = ({ navigation, route }) => {
 
   return (
     <View style={styles.latar}>
-      <MapView style={styles.peta}/>
+
       <View style={styles.bungkus}>
-        <View style={{ flexDirection:'row', marginBottom: 10 }}>
-            <Image source={DPkartu} style={styles.foto} />
-            <View style={{  flex: 3}}>
-              <Text style={{fontSize:18, fontWeight:'bold', color:IjoTua}}>
+        <View style={{ flexDirection:'row', marginBottom: 10, justifyContent:'space-between', alignItems:'center' }}>
+            <View>
+              <Text style={{fontSize:20, fontWeight:'bold', color:IjoTua}}>
                 Sayur Aa Anri
               </Text>
-              <Text style={{color: Ijo}}>Sedang menuju lokasi kamu</Text>
               <View style={{flexDirection:'row'}}>
                   <Image source={Call} style={styles.icon} />
                   <Image source={Chat} style={styles.icon} />
               </View>
             </View>
+            <Image source={DPkartu} style={styles.foto} />
         </View>
+        <GarisBatas/>
         <View style={{marginBottom: 20}}>
             <Text style={{fontSize:14, fontWeight:'bold', color:IjoTua}}>Tujuan Lokasi</Text>
-            <Text>Jl. nuabsua sahushaus sauhauhsau usahusah sbausbau ashsua ncnsnicns csnidcnsoc acnscuodsscnu</Text>
+            <Text numberOfLines={3}>Jl. nuabsua sahushaus sauhauhsau usahusah sbausbau ashsua ncnsnicns csnidcnsoc acnscuodsscnu</Text>
         </View>
         <Text style={{fontSize:16, fontWeight:'bold', color:Ijo, textDecorationLine:'underline', textAlign:'center'}}
         
@@ -51,30 +53,26 @@ export default OtwScreen
 const styles = StyleSheet.create({
     latar:{
         flex: 1,
-    },
-    peta:{
-        width: '100%',
-        height: '70%',
+        backgroundColor: IjoTua,
     },
     bungkus:{
         backgroundColor: Kuning,
         position:'absolute',
         bottom: 0,
-        width: '100%',
+        width: width,
+        height: height * 0.35,
         padding: 20,
         borderTopLeftRadius: 20,
         borderTopRightRadius: 20,
     },
     foto:{
-        width: width * 0.25,
-        height: width * 0.25,
+        width: width * 0.2,
+        height: width * 0.2,
         borderRadius: 10,
-        marginRight: 10,
-        flex: 1,
     },
     icon:{
-        width: width * 0.1,
-        height: width * 0.1,
+        width: width * 0.10,
+        height: width * 0.10,
         marginVertical: 5,
         marginRight: 20,
     },
