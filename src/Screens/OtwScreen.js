cc6ba566e68f5c5b49ba98e8d84e9b129f36af6d
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-10-06 11:41:03 +0700

    Masang variabel di otw screen

--- src/Screens/OtwScreen.js
+++ src/Screens/OtwScreen.js
@@ -1,5 +1,5 @@
 import { Image, StyleSheet, Text, View, Dimensions, Pressable } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import MapView, { Marker } from 'react-native-maps'
 import { Ijo, IjoMint, IjoTua, Kuning, Putih } from '../Utils/Warna'
 import { DPkartu } from '../assets/Images/Index'
@@ -7,49 +7,137 @@ import { Call, Chat } from '../assets/Icons/Index'
 import { Tiba, Perjalanan, Load1, Load2, Load3 } from '../assets/Images/Index'
 import GarisBatas from '../Components/GarisBatas';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { getFirestore, doc, getDoc, onSnapshot } from 'firebase/firestore';
+import { app } from '../../Firebase/config';
+
 
 
 const { height, width } = Dimensions.get('window')
 
 const OtwScreen = ({ navigation, route }) => {
 
+  const db = getFirestore(app)
+
+  // const [panggilan, setPanggilan] = useState("Diterima")
+  
+  const [namatoko, setNamatoko] = useState();
+  const [phonemitra, setPhonemitra] = useState();
+  const [alamat_pelanggan, setAlamat_pelanggan] = useState();
+  const [estimasi_waktu, setEstimasi_waktu] = useState();
+  const [jarak, setJarak] = useState();
+
+  const telepon = () => {
+    Linking.openURL(`tel:${phonemitra}`);
+  };
+
+  const sms = () => {
+    Linking.openURL(`sms:${phonemitra}`);
+  };
+
   // const { 
   //   id_transaksi
   //    } = route.params;
 
+  //   useEffect(() =>{ 
+  //   async function getPanggilan(){
+  //     try{
+  //       const unsubscribe = onSnapshot(doc(db, "transaksi", id_transaksi), (doc) => {
+  //       setPanggilan(doc.data().panggilan);
+  //       console.log('getPanggilan jalan (Home Screen)')
+  //         // Respond to data
+  //         // ...
+  //       });
+  //       //unsubscribe();
+  //     } catch (err){
+  //       Alert.alert('There is an error.', err.message)
+  //     }
+  //   }
+  //   getPanggilan();
+  // },[panggilan])
+
+    useEffect(() =>{ 
+    async function getDetailTransaksi(){
+      const docRef = doc(db, "transaksi", id_transaksi);
+      const docSnap = await getDoc(docRef);
+      
+      if (docSnap.exists()) {
+        setNamatoko(docSnap.data().namatoko);
+        setPhonemitra(docSnap.data().phonemitra);
+        setAlamat_pelanggan(docSnap.data().alamat_pelanggan);
+        setEstimasi_waktu(docSnap.data().estimasi_waktu);
+        setJarak(docSnap.data().jarak);
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    }
+    getDetailTransaksi();
+  },[])
 
 
   return (
     <View style={styles.latar}>
-      <View>
-        <Image source={Perjalanan} style={styles.gambar}/>
-        <Image source={Load1} style={styles.load}/>
-        <Text style={styles.tulisan}>Mitra sedang menuju lokasi kamu</Text>
-      </View>
+        <View>
+          <Image source={Perjalanan} style={styles.gambar}/>
+          <Image source={Load1} style={styles.load}/>
+          <Text style={styles.tulisan}>Mitra sedang menuju lokasi kamu</Text>
+          <Text style={styles.tulisan}>Estimasi sampai {estimasi_waktu} dalam jarak {jarak}</Text>
+        </View>
+      {/* { panggilan == "Diterima" ? (
+        <View>
+          <Image source={Perjalanan} style={styles.gambar}/>
+          <Image source={Load1} style={styles.load}/>
+          <Text style={styles.tulisan}>Mitra sedang menuju lokasi kamu</Text>
+        </View>
+        ): panggilan == "Sudah Sampai" ? (
+        <View>
+          <Image source={Tiba} style={styles.gambar}/>
+          <Image source={Load2} style={styles.load}/>
+          <Text style={styles.tulisan}>Mitra sudah sampai lokasi kamu, yuk belanja!</Text>
+        </View>
+        ) : (
+          <View>
+            <Image source={Perjalanan} style={styles.gambar}/>
+            <Image source={Load3} style={styles.load}/>
+            <Text style={styles.tulisan}>Terima kasih sudah berbelanja bersama kami</Text>
+          </View>
+        )
+      } */}
 
       <View style={styles.bungkus}>
-        <View style={{ flexDirection:'row', marginBottom: 10, justifyContent:'space-between', alignItems:'center' }}>
-            <View style={{flex: 6}}>
+        <View style={{ flexDirection:'row', marginBottom: 10, alignItems:'center' }}>
+            <View style={{flex: 2}}>
               <Text style={{fontSize:20, fontWeight:'bold', color:IjoTua}} numberOfLines={1}>
-                Sayur Segar Dramga
+                {namatoko}
               </Text>
-              <View style={{flexDirection:'row'}}>
-                  <Image source={Call} style={styles.icon} />
-                  <Image source={Chat} style={styles.icon} />
-              </View>
             </View>
-            <Image source={DPkartu} style={styles.foto} />
+            <View style={{flexDirection:'row', flex: 1}}>
+              <Pressable onPress={telepon}>
+                  <Image source={Call} style={styles.icon} />
+              </Pressable>
+              <Pressable onPress={sms}>
+                  <Image source={Chat} style={styles.icon} />
+              </Pressable>
+            </View>
         </View>
         <GarisBatas/>
         <View style={{marginBottom: 20}}>
             <Text style={{fontSize:14, fontWeight:'bold', color:IjoTua}}>Tujuan Lokasi</Text>
-            <Text numberOfLines={3}>Jl. nuabsua sahushaus sauhauhsau usahusah sbausbau ashsua ncnsnicns csnidcnsoc acnscuodsscnu</Text>
+            <Text numberOfLines={3}>{alamat_pelanggan}</Text>
         </View>
-        <Text style={{fontSize:16, fontWeight:'bold', color:Ijo, textDecorationLine:'underline', textAlign:'center'}}
-        
-        >
-           Batalkan Pesanan
-        </Text>
+
+        {/* { panggilan == "Diterima" &&
+          <Text style={{fontSize:16, fontWeight:'bold', color:Ijo, textDecorationLine:'underline', textAlign:'center'}}
+          
+          >
+            Batalkan Pesanan
+          </Text>
+        } */}
+          <Text style={{fontSize:16, fontWeight:'bold', color:Ijo, textDecorationLine:'underline', textAlign:'center'}}
+          
+          >
+            Batalkan Pesanan
+          </Text>
       </View>
       <Pressable style={styles.kembali} onPress={()=> navigation.navigate('HomeScreen')}>
             <Ionicons name="chevron-back-circle-outline" size={40} color={Putih} />
@@ -86,7 +174,7 @@ const styles = StyleSheet.create({
         width: width * 0.10,
         height: width * 0.10,
         marginVertical: 5,
-        marginRight: 20,
+        marginLeft: 15,
     },
     gambar:{
       width: width * 0.9,
