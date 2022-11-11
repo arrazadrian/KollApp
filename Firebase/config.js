88cf92f5587dfd504585657ac2db60ae1923d8a5
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-11-11 16:34:09 +0700

    nambah admin dan gitignore

--- Firebase/config.js
+++ Firebase/config.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+import { initializeApp } from 'firebase-admin/app';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
